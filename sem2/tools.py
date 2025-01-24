4c35b7267ccc631379006ebfa6217f465428da4e
odu1ros <odu1chique@mail.ru>
2025-01-24 23:54:11 +0300

    first seminars

---
+++ sem2/tools.py
@@ -0,0 +1,18 @@
+import cv2
+import PIL
+import numpy as np
+import matplotlib.pyplot as plt
+
+def display(window, img):
+    cv2.imshow(window, img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+def display_inline(img, grey=False):
+    fig = plt.figure(figsize=(10, 6))
+    if grey:
+        plt.imshow(img, cmap='gray')
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.imshow(img)
+    plt.show()
